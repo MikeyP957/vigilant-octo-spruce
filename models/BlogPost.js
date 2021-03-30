cc1f53a407e696d7e924317e310f858b255c4f58
Mikey P <peraram2@gmail.com>
2021-03-29 20:46:39 -0700

    BlogPost init values,  thinking about  comment pt

---
+++ models/BlogPost.js
@@ -0,0 +1,37 @@
+const { Model, DataTypes} = require('sequelize');
+const sequelize = require('../config/connection');
+
+class BlogPost extends Model{}
+
+BlogPost.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+        //how to store an entire message
+        date_created: {
+            type: DataTypes.DATE,
+            allowNull: false,
+            defaultValue: DataTypes.NOW,
+        },
+        user_id: {
+            type: DataTypes.INETGER,
+            references: {
+                model: 'user',
+                key: 'id',
+            }
+        },
+    },
+    {
+        sequelize,
+        timestamps: false,
+        freezeTableName: true,
+        underscored: true,
+        modelName:'project',
+    }
+)
+
+module.exports = BlogPost; 

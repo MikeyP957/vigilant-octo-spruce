5be37f57fb7ace36ac191e0abff9751032f529c6
Mikey P <peraram2@gmail.com>
2021-04-03 22:09:25 -0700

    del button handler

--- public/js/profile.js
+++ public/js/profile.js
@@ -22,7 +22,19 @@ const newFormHandler = async (event) => {
 };
 const delButtonHandler = async (event) => {
     event.preventDefault();
+    if(event.target.hasAttribute('data-id')) {
+        const id = event.target.getAttribute('data-id');
 
+        const response = await fetch(`/api/blogPosts/${id}`, {
+            method:'DELETE',
+        });
+
+        if (response.ok) {
+            document.location.replace('/profile');
+        } else {
+            alert('Failed to delete blog post')
+        }
+    }
 };
 
 document

38e92e869752ec393a177e6919af8f568e43db9c
Mikey P <peraram2@gmail.com>
2021-04-03 21:35:05 -0700

    comment out comment until blog and user working

--- models/index.js
+++ models/index.js
@@ -1,6 +1,6 @@
 const User = require('./User');
 const BlogPost = require('./BlogPost');
-const Comment = require('./Comment');
+// const Comment = require('./Comment');
 
 
 //Blog Post belongs to a user, and a user has many blog posts, linked by user_id on blogPost
@@ -12,24 +12,24 @@ User.hasMany(BlogPost, {
     foreignKey: 'user_id',
 });
 
-Comment.belongsTo(User, {
-    foreignKey: 'user_id'
-});
+// Comment.belongsTo(User, {
+//     foreignKey: 'user_id'
+// });
 
-User.hasMany(Comment, {
-    foreignKey: 'user_id',
-});
+// User.hasMany(Comment, {
+//     foreignKey: 'user_id',
+// });
 
 //A blog post can have many comments, which are linked with the commentTag, CommentTag has a user_id and blogPost_id
-Comment.belongsTo(BlogPost, {
-    foreignKey: 'blogPost_id',
-});
+// Comment.belongsTo(BlogPost, {
+//     foreignKey: 'blogPost_id',
+// });
 
-BlogPost.hasMany(Comment, {
-    foreignKey: 'blogpost_id'
-});
-
-
+// BlogPost.hasMany(Comment, {
+//     foreignKey: 'blogpost_id'
+// });
 
 
-module.exports = {User, BlogPost, Comment};
+
+//add Comment model
+module.exports = {User, BlogPost};

922f3a6f8d3b672ea3376f20587fa795a12a0739
Mikey Perara <mikeyp@Mikeys-MacBook.local>
2021-04-06 00:57:13 -0700

    creating comments dependencies and routes

--- models/index.js
+++ models/index.js
@@ -1,6 +1,6 @@
 const User = require('./User');
 const BlogPost = require('./BlogPost');
-// const Comment = require('./Comment');
+const Comment = require('./Comment');
 
 
 //Blog Post belongs to a user, and a user has many blog posts, linked by user_id on blogPost
@@ -14,24 +14,24 @@ BlogPost.belongsTo(User, {
 
 
 
-// Comment.belongsTo(User, {
-//     foreignKey: 'user_id'
-// });
+Comment.belongsTo(User, {
+    foreignKey: 'user_id'
+});
 
-// User.hasMany(Comment, {
-//     foreignKey: 'user_id',
-// });
+User.hasMany(Comment, {
+    foreignKey: 'user_id',
+});
 
-//A blog post can have many comments, which are linked with the commentTag, CommentTag has a user_id and blogPost_id
-// Comment.belongsTo(BlogPost, {
-//     foreignKey: 'blogPost_id',
-// });
+// A blog post can have many comments, which are linked with the commentTag, CommentTag has a user_id and blogPost_id
+Comment.belongsTo(BlogPost, {
+    foreignKey: 'blogPost_id',
+});
 
-// BlogPost.hasMany(Comment, {
-//     foreignKey: 'blogpost_id'
-// });
+BlogPost.hasMany(Comment, {
+    foreignKey: 'blogpost_id'
+});
 
 
 
 //add Comment model
-module.exports = {User, BlogPost};
+module.exports = {User, BlogPost, Comment};
